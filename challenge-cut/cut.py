1f004014836a2c56732c39c42bcdc353bfd8670e
uttaran-das <dasuttaran18@gmail.com>
2024-09-09 02:28:35 +0530

    cut tool (step 2)

--- challenge-cut/cut.py
+++ challenge-cut/cut.py
@@ -1,10 +1,10 @@
 import argparse
 
-def cut_field(file_path, field_number):
+def cut_field(file_path, field_number, delimiter):
     try:
         with open(file_path, 'r') as file:
             for line in file:
-                fields = line.strip().split('\t');
+                fields = line.strip().split(delimiter);
                 if len(fields) >= field_number:
                     print(fields[field_number-1])
                 else:
@@ -15,12 +15,13 @@ def cut_field(file_path, field_number):
         print(f"Error: The file '{file_path}' was not found.")
 
 def main():
-    parser = argparse.ArgumentParser(description="Extract a specified field from each line of a tab-separated file.")
+    parser = argparse.ArgumentParser(description="Extract a specified field from each line of a file with delimeter.")
     parser.add_argument('-f', '--field', type=int, default=1, help='Field number to extract (default is 1)')
+    parser.add_argument('-d', '--delimiter', type=str, default='\t', help="Delimiter character to use. Default is tab.")
     parser.add_argument('file', help='Path to the tab-separated file')
     args = parser.parse_args()
 
-    cut_field(args.file, args.field)
+    cut_field(args.file, args.field, args.delimiter)
 
 if __name__ == "__main__":
     main()
