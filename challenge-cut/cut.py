a8233a5a5ef1061b87cf8bb0385b43f91ee2a090
uttaran-das <dasuttaran18@gmail.com>
2024-09-09 02:52:54 +0530

    cut tool (step 3)

--- challenge-cut/cut.py
+++ challenge-cut/cut.py
@@ -1,27 +1,27 @@
 import argparse
 
-def cut_field(file_path, field_number, delimiter):
+def cut_field(file_path, field_list, delimiter):
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 fields = line.strip().split(delimiter);
-                if len(fields) >= field_number:
-                    print(fields[field_number-1])
-                else:
-                    print("")
+                extracted_fields = [fields[i - 1] for i in field_list if 1 <= i <= len(fields)]
+                print(delimiter.join(extracted_fields))
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
     except Exception as e:
-        print(f"Error: The file '{file_path}' was not found.")
+        print(f"An error occurred: {e}")
 
 def main():
-    parser = argparse.ArgumentParser(description="Extract a specified field from each line of a file with delimeter.")
-    parser.add_argument('-f', '--field', type=int, default=1, help='Field number to extract (default is 1)')
+    parser = argparse.ArgumentParser(description="Extract a specified field from each line of a file with delimiter.")
+    parser.add_argument('-f', '--field', type=str, default=1, help='Field number to extract (default is 1). Comma or whitespace separated.')
     parser.add_argument('-d', '--delimiter', type=str, default='\t', help="Delimiter character to use. Default is tab.")
     parser.add_argument('file', help='Path to the tab-separated file')
     args = parser.parse_args()
 
-    cut_field(args.file, args.field, args.delimiter)
+    field_list = [int(field) for field in args.field.split(',' if ',' in args.field else ' ')]
+
+    cut_field(args.file, field_list, args.delimiter)
 
 if __name__ == "__main__":
     main()
